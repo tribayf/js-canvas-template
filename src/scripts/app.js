f5374558f65a85837546d120b0644186648f74c1
Tristan Bayfield <tristan.bayfield@gmail.com>
2020-06-09 17:13:34 -0700

    Formatting + improvements

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,59 +2,72 @@
 
 class App
 {
-  static frameId_ = null
-  static canvas_  = document.getElementById("c")
-  static context_ = document.getElementById("c").getContext("2d")
+/* ------------------------------------------------------------------------------------ *\
+   Public API
+\* ------------------------------------------------------------------------------------ */
 
-  static get canvas() { return this.canvas_ }
+  static get canvas()  { return this.canvas_  }
   static get context() { return this.context_ }
 
   static start()
   {
     let state = this.init({ appState: {} })
-    this.frameId_ = window.requestAnimationFrame(time => this.loop({ appState: state }))
+    this.frameId_ = window.requestAnimationFrame(_ => this.__loop__({ appState: state }))
   }
 
+  static stop()
+  {
+    window.cancelAnimationFrame(this.frameId_)
+    this.frameId_ = null
+  }
+
+/* ------------------------------------------------------------------------------------ *\
+   Client Supplied Methods
+\* ------------------------------------------------------------------------------------ */
+
+  /* Optional */
   static init ({ appState: state }) { return state }
 
+  /* Required */
   static update({ deltaTime: dt, appState: state })
   {
     const sig = 'static update({ deltaTime:dt, appState:state }): state'
     throw new Error(`\n\tmissing required method: "${sig}"`)
   }
 
+  /* Required */
   static draw({ appState: state })
   {
     const sig = 'static draw({ appState:state }): void'
     throw new Error(`\n\tmissing required method: "${sig}"`)
   }
 
-  static stop()
-  {
-    window.cancelAnimationFrame(this.frameId_)
-  }
+/* ------------------------------------------------------------------------------------ *\
+   Private API
+\* ------------------------------------------------------------------------------------ */
 
-  static loop({ currentTime: t1 = 0, previousTime: t0 = 0, appState: s0 })
+  static frameId_ = null
+  static canvas_  = document.getElementById("c")
+  static context_ = document.getElementById("c").getContext("2d")
+
+  static __loop__({ currentTime: t1 = 0, previousTime: t0 = 0, appState: s0 })
   {
+    if (this.frameId_ == null) return
+
     let s1 = this.update({ deltaTime: t1-t0, appState: s0 })
 
     this.draw({ appState: s1 })
 
-    requestAnimationFrame(time =>
-      this.loop({ currentTime: time, previousTime: t1, appState: s1 }))
+    this.frameId_ = requestAnimationFrame(time =>
+      this.__loop__({ currentTime: time, previousTime: t1, appState: s1 }))
   }
 }
 
-export function MakeApp(cls)
+export function MakeApp(Cls)
 {
-  function decorate(cls, prop)
-  {
-    if (cls.hasOwnProperty(prop))
-      Object.defineProperty(App, prop, Object.getOwnPropertyDescriptor(cls, prop))
-  }
-
-  ['draw', 'update', 'init'].map((prop) => decorate(cls, prop))
-
-  return App
+  let Wrapped = {}
+  Object.defineProperties(Wrapped, Object.getOwnPropertyDescriptors(App))
+  Object.defineProperties(Wrapped, Object.getOwnPropertyDescriptors(Cls))
+  return Wrapped
 }
 
