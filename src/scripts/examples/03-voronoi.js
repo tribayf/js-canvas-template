fd1f76269f9d2e7835bfe8cedcc4eafe60088f34
Tristan Bayfield <tristan.bayfield@gmail.com>
2020-07-04 22:25:49 -0700

    Stub out voronoi example

---
+++ src/scripts/examples/03-voronoi.js
@@ -0,0 +1,19 @@
+'use strict'
+
+export default class Voronoi
+{
+  static init(config)
+  {
+    return state
+  }
+
+  static update({ deltaTime: dt, appState: state })
+  {
+    return state
+  }
+
+  static draw({ appState: state })
+  {
+
+  }
+}
