22d73485289bde06fc1be8942c3e84b14c986a80
Tristan Bayfield <tristan.bayfield@gmail.com>
2020-07-06 20:27:00 -0700

    Add voronoi site rendering

--- src/scripts/examples/03-voronoi.js
+++ src/scripts/examples/03-voronoi.js
@@ -6,34 +6,51 @@ export default class Voronoi
 {
   static init(config)
   {
+    const [w, h, margin] = [640, 480, Math.floor(0.1*640)]
+
+    this.canvas.width  = w
+    this.canvas.height = h
+
     const siteCount = config.siteCount || 10,
-          xmin  = config.xmin || 0,
-          xmax  = config.xmax || this.canvas.width,
-          ymin  = config.ymin || 0,
-          ymax  = config.ymax || this.canvas.height,
+          xmin  = config.xmin || margin,
+          xmax  = config.xmax || w - 2*margin,
+          ymin  = config.ymin || margin,
+          ymax  = config.ymax || h - 2*margin,
           sites = generateSites(siteCount, xmin, xmax, ymin, ymax),
           queue = new PriorityQueue(compare)
 
     sites.map(s => queue.push(s))
 
-    return { queue }
+    return { sites, queue }
   }
 
   static update({ deltaTime: dt, appState: state })
   {
-    const s = state.queue.pop()
-
-    if (siteEvent(s))
-      state.beachline.add(s)
-    else
-      state.beachline.remove(s)
+    // const s = state.queue.pop()
+    // if (siteEvent(s))
+    //   state.beachline.add(s)
+    // else
+    //   state.beachline.remove(s)
 
     return state
   }
 
   static draw({ appState: state })
   {
+    this.clear()
+    this.drawSites(state.sites)
+  }
 
+  static drawSites(sites)
+  {
+    this.context.fillStyle = 'white'
+    sites.map(([x, y]) => this.context.fillRect(x, y, 2, 2))
+  }
+
+  static clear()
+  {
+    this.context.fillStyle = 'black'
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
   }
 }
 
